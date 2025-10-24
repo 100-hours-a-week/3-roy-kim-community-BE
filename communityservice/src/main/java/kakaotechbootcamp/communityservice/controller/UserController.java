80aa5e888f91925130634c8f8ab12c3c146fd114
dokkang01 <kimd02.dev@gmail.com>
2025-10-24 20:42:38 +0900

    기능 추가: 이메일, 닉네임 중복 검사

--- communityservice/src/main/java/kakaotechbootcamp/communityservice/controller/UserController.java
+++ communityservice/src/main/java/kakaotechbootcamp/communityservice/controller/UserController.java
@@ -2,6 +2,7 @@ package kakaotechbootcamp.communityservice.controller;
 
 import kakaotechbootcamp.communityservice.dto.*;
 import kakaotechbootcamp.communityservice.entity.User;
+import kakaotechbootcamp.communityservice.repository.UserRepository;
 import kakaotechbootcamp.communityservice.service.UserService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -11,39 +12,61 @@ import org.springframework.http.HttpStatus;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost"}, methods = {RequestMethod.POST, RequestMethod.OPTIONS}, allowedHeaders = "*")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost"},
+        methods = {RequestMethod.POST,
+                RequestMethod.GET,
+                RequestMethod.OPTIONS},
+        allowedHeaders = "*")
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("signup")
-    public UserResponse create(@RequestBody CreateUserRequest request){
+    @PostMapping("/signup")
+    public UserResponse create(@RequestBody CreateUserRequest request) {
         User saved = userService.signUp(request.getEmail(), request.getPassword(), request.getPasswordCheck(), request.getNickname(), request.getProfilePicture());
         return UserResponse.of(saved);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest){
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
         String loginResult = userService.login(loginRequest);
         return ResponseEntity.ok(loginResult);
     }
+
     @GetMapping(("/{id}"))
-    public UserResponse findById(@PathVariable Long id){
+    public UserResponse findById(@PathVariable Long id) {
         return UserResponse.of(userService.findById(id));
     }
 
     @PatchMapping("/{id}")
-    public UserResponse update(@PathVariable Long id, @RequestBody UpdateUserRequest request){
+    public UserResponse update(@PathVariable Long id, @RequestBody UpdateUserRequest request) {
         User updatedUser = userService.update(id, request.getNickname(), request.getProfilePicture());
         return UserResponse.of(updatedUser);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
+    public void delete(@PathVariable Long id) {
         userService.delete(id);
+    }
+
+    /**
+     * 이메일 중복검사
+     */
+    @GetMapping("/check-email")
+    public boolean checkEmail(@RequestParam String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    /**
+     * 닉네임 중복검사
+     */
+    @GetMapping("/check-nickname")
+    public boolean checkNickname(@RequestParam String nickname) {
+        return userRepository.existsByNickname(nickname);
     }
 
     @Data
@@ -54,6 +77,7 @@ public class UserController {
         private String nickname;
         private String profilePicture;
     }
+
     @Data
     public static class UserResponse {
         private Long id;
@@ -70,6 +94,7 @@ public class UserController {
             );
 
         }
+
         public UserResponse(long id, String email, String nickname, String profilePicture) {
             this.id = id;
             this.email = email;
@@ -84,9 +109,4 @@ public class UserController {
         private String profilePicture;
     }
 
-//    @Data
-//    public static class LoginRequest {
-//        private String email;
-//        private String password;
-//    }
 }
