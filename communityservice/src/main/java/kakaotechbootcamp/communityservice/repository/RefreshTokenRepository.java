f97472bdc0bb9d9446ff1f6f15a9010a4b018ead
dokkang01 <kimd02.dev@gmail.com>
2025-10-30 11:41:36 +0900

    RT repository 구현

---
+++ communityservice/src/main/java/kakaotechbootcamp/communityservice/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package kakaotechbootcamp.communityservice.repository;
+
+import kakaotechbootcamp.communityservice.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByTokenAndRevokedFalse(String token);
+    void deleteByUserId(Long userId);
+}
