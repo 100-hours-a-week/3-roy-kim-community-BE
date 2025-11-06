eb5491e170edb085cf0531cfdc0156d050e59ae0
Ubuntu <ubuntu@ip-172-31-3-62.ap-southeast-2.compute.internal>
2025-11-06 07:34:14 +0000

    AWS 주소로 변경

--- communityservice/src/main/java/kakaotechbootcamp/communityservice/config/WebCorsConfig.java
+++ communityservice/src/main/java/kakaotechbootcamp/communityservice/config/WebCorsConfig.java
@@ -8,7 +8,7 @@ public class WebCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://ec2-16-176-163-210.ap-southeast-2.compute.amazonaws.com:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
